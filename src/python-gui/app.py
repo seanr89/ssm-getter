9606779cdaae591bb3b2fd455b2acc05aa366dba
seanr89 <srafferty89@gmail.com>
2024-12-30 22:11:13 +0000

    Enhance GUI functionality by increasing window size, adding label update feature, and modifying button actions

--- src/python-gui/app.py
+++ src/python-gui/app.py
@@ -6,8 +6,15 @@ from tkinter import simpledialog
 # Initialize the main window
 root = ctk.CTk()
 root.title("CustomTkinter Side-by-Side Frames Example")
-root.geometry("620x300")
+root.geometry("620x400")
 root.resizable(False, False)
+
+#region global-vars
+
+# Global variable to store the selected value 
+selected_account = None
+
+#endregion
 
 #region Arrays
 
@@ -33,7 +40,10 @@ colours = [
 
 # Add a combobox and set the initial value + callback event
 def combobox_callback_account(choice):
+    global selected_account 
+    selected_account = comboboxacc.get()
     print("combobox account dropdown choice:", choice)
+    print("combobox account dropdown selectedval:", selected_account)
 
 def combobox_callback_env(choice):
     print("combobox env dropdown choice:", choice)
@@ -41,18 +51,29 @@ def combobox_callback_env(choice):
 def combobox_callback_colour(choice):
     print("combobox colour dropdown choice:", choice)
 
+# Function to update the label text 
+def update_label():
+    print("Label Updated!") 
+    label.configure(text="Label Updated!")
+    create_path_prefix()
+
+def create_path_prefix():
+    user = "Alice"
+    folder = "Documents"
+    file = "example.txt"
+    path = f"C:/Users/{user}/{folder}/{file}"
+    print(f"The full path is: {path}")
+
+
 #endregion
+
+
 
 
 # Function to handle button press event and close the app
 def on_button_press():
-    messagebox.showinfo("Information", "This is a Tkinter dialog!")
-
-def on_button_press_dialog():
-    user_input = simpledialog.askstring("Input", "Please enter some text:") 
-    if user_input is not None: 
-        print(f"User input: {user_input}")
-
+    print("Button Pressed!")
+    # messagebox.showinfo("Information", "This is a Tkinter dialog!")
 
 # Create two frames
 left_frame = ctk.CTkFrame(root, width=150, height=290, fg_color="lightblue")
@@ -77,11 +98,16 @@ comboboxcolour = ctk.CTkComboBox(master=left_frame,
 comboboxcolour.pack(padx=20, pady=10)
 
 # Add a button to the left frame
-button = ctk.CTkButton(left_frame, text="Open Message", command=on_button_press)
+button = ctk.CTkButton(left_frame, text="Search", command=on_button_press)
 button.pack(pady=20, padx=20)
 
-# dialogButton = ctk.CTkButton(left_frame, text="Open Dialog", command=on_button_press_dialog)
-# dialogButton.pack(pady=20)
+# Create a button that updates the label 
+updatebtn = ctk.CTkButton(left_frame, text="Update Label", command=update_label) 
+updatebtn.pack(pady=20)
+
+# Create a customtkinter label 
+label = ctk.CTkLabel(root, text="Hello, CustomTkinter!", width=170, height=50, corner_radius=5) 
+label.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
 # Run the application
 root.mainloop()
